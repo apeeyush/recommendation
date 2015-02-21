7299a4d759dee2547ea2954831916f8d56f9a95b
apeeyush <peeyushagarwal1994@gmail.com>
2015-02-22 02:00:19 +0530

    Scrape : add sitemap xml

--- scrape/jabong/download_urls.py
+++ scrape/jabong/download_urls.py
@@ -1,6 +1,11 @@
 import urllib
+import os
+
 link = 'http://www.jabong.com/sitemapgen/sitemapxml/product/id/'
-for i in range (20,24):
-	urllib.urlretrieve(link+str(i),filename=str(i)+".xml")
+for i in range (20,21):
+    directory = str(i)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    urllib.urlretrieve(link+str(i),filename=directory+'/'+str(i)+".xml")
 
 # XPath : /urlset/url[1]/loc
